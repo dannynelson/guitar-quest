0817b8fab5b3a0ba8b50fb986bc2e821deb9c6d6
dannynelson <danny@goodeggs.com>
2015-12-30 12:04:59 -0800

    Add toggle to navbar

--- react/routes/App/components/Navbar/index.js
+++ react/routes/App/components/Navbar/index.js
@@ -9,11 +9,14 @@ class AppNavigation extends Component {
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>GuitarQuest</Navbar.Brand>
+          <Navbar.Toggle />
         </Navbar.Header>
-        <Nav>
-          <NavItem className="pieces" eventKey={1} onClick={this.props.onNavItemClick.bind(null, '/pieces_by_level/1')}>Pieces</NavItem>
-          <NavItem className="confirm email" eventKey={1} onClick={this.props.onNavItemClick.bind(null, '/confirm_email')}>Hello</NavItem>
-        </Nav>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem className="pieces" eventKey={1} onClick={this.props.onNavItemClick.bind(null, '/pieces_by_level/1')}>Pieces</NavItem>
+            <NavItem className="confirm email" eventKey={1} onClick={this.props.onNavItemClick.bind(null, '/confirm_email')}>Hello</NavItem>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     )
   }
